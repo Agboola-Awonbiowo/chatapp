1c429545b2b24353768a30611d08a7a7c86def90
agboola <agboolanathaniel@yahoo.com>
2022-08-07 07:37:03 +0100

    implements login functionality

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+import ChatScreen from './ChatScreen';
 
 function LoginScreen() {
-  
+  const [userName, setUserName] = useState('');
+  const handleSubmit = () => {
+    if (localStorage.setItem('name', userName)) {
+      return <ChatScreen />;
+    }
+  };
   return (
-    <form className="d-flex align-items-center justify-content-center vh-100">
+    <form
+      onSubmit={handleSubmit}
+      className="d-flex align-items-center justify-content-center vh-100"
+    >
       <div>
         <div>
-          <input type="text" className='p-2' placeholder="Enter Username" />
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            className="p-2"
+            placeholder="Enter Username"
+          />
         </div>
-        <Button variant="primary" className="btn-primary w-100 mt-3">
+        <Button
+          variant="primary"
+          disabled={!userName}
+          className="btn-primary w-100 mt-3"
+          type="submit"
+        >
           Submit
         </Button>
       </div>
