30f31882e6cf190de9870ed773fdacd779fe1876
agboola <agboolanathaniel@yahoo.com>
2022-08-07 05:44:48 +0100

    switch branch

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LoginScreen() {
+  return (
+    <form>
+      <div>
+        <input type="text" placeholder="Enter Username" />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default LoginScreen;
